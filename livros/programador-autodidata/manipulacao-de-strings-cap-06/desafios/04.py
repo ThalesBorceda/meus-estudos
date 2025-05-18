e092d7f32aa6753516d0e972e0e2623dab665021
Thales Borceda <bio.thales@gmail.com>
2025-05-18 19:14:44 -0300

    Update 04.py

--- livros/programador-autodidata/manipulacao-de-strings-cap-06/desafios/04.py
+++ livros/programador-autodidata/manipulacao-de-strings-cap-06/desafios/04.py
@@ -1,1 +1,4 @@
+#Pegue a string "Where now? Who now? When now?" e chame um método que retorne a lsita com essa
+#aparência ["Where now?", "Who now?", "When now?"]. 
 
+"Where now?.Who now?.When now?".split('.')
