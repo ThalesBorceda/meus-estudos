c9e17b7ca655adf5de7021f314856567605c8543
Thales Borceda <bio.thales@gmail.com>
2025-05-18 19:50:52 -0300

    Create 08.py

---
+++ livros/programador-autodidata/manipulacao-de-strings-cap-06/desafios/08.py
@@ -0,0 +1,6 @@
+#Encontra um diálogo em seu livro favorito (contendo aspas) e transforme-o em uma string
+#No momento estou lendo Dom Quixote, então vou pegar dele 
+
+string = " - É - disse o barbeiro - As segras (ou \"façanhas\") de Esplandião, filho legítimo de Amadis de Gaula"
+
+print(string)
