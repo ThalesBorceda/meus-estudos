423ce20c71e555e0fc2fad2010e5d7e34f6c1132
Thales Borceda <bio.thales@gmail.com>
2025-05-18 19:35:45 -0300

    Update 06.py

--- livros/programador-autodidata/manipulacao-de-strings-cap-06/desafios/06.py
+++ livros/programador-autodidata/manipulacao-de-strings-cap-06/desafios/06.py
@@ -1,1 +1,5 @@
+# Substitua cada ocorrência de "s" em "A screaming comes acros the sky." por um cifrão 
 
+phrase = str("A screaming comes acros the sky.")
+phrase = phrase.replace("s", "$")
+print(phrase)
