3476fc47f9ca2cc54019104328dd348ab6218e0f
Thales Borceda <bio.thales@gmail.com>
2025-05-18 20:02:00 -0300

    Update 09.py

--- livros/programador-autodidata/manipulacao-de-strings-cap-06/desafios/09.py
+++ livros/programador-autodidata/manipulacao-de-strings-cap-06/desafios/09.py
@@ -1,1 +1,8 @@
+#Cria a string "three three three" usando a concatenação e, em seguida, crie-a novamente usando a 
+#mutiplicação
 
+# concatenação 
+"three " + "three " + "three" 
+
+#multiplicação
+"three " * 3 
