99967ddd019d51171edbf49f06901d849ea5e9f2
Thales Borceda <bio.thales@gmail.com>
2025-05-18 19:10:48 -0300

    Update 03.py

--- livros/programador-autodidata/manipulacao-de-strings-cap-06/desafios/03.py
+++ livros/programador-autodidata/manipulacao-de-strings-cap-06/desafios/03.py
@@ -1,1 +1,4 @@
+#Use um método para tornar a string "aldous Huxley was born in 1984." gramaticalmente correta
+#fazendo com que a primeira letra da frase seja maiúscula
 
+"aldous Huxley was born in 1984".capitalize()
