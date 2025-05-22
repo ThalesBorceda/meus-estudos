2d6efbd642bf9c9b7c45023ac17c149d997aaa80
Thales Borceda <bio.thales@gmail.com>
2025-05-22 10:36:37 -0300

    Create cubed.py

---
+++ livros/programador-autodidata/modulos - cap 07/desafios/modulos/cubed.py
@@ -0,0 +1,4 @@
+  # Crie um módulo chamado cubed com uma função que receba um número como parâmetro e retorneo número ao cubo. Importe e chame a função a partir de outro módulo 
+
+def cubed(number):
+    return number ** 3
