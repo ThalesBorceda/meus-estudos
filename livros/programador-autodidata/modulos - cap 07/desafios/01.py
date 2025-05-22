0b9e6bdd50f564a0a1344233356316f36ee965be
Thales Borceda <bio.thales@gmail.com>
2025-05-22 10:35:44 -0300

    Create 01.py

---
+++ livros/programador-autodidata/modulos - cap 07/desafios/01.py
@@ -0,0 +1,8 @@
+# Chame uma função diferente do módulo statistics
+import statistics 
+
+numbers = [10, 25, 10, 42, 55, 243, 10, 938]
+
+print(statistics.mode(numbers))
+
+print(statistics.median_high(numbers))
